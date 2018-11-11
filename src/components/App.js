65b4d2772803ab337cca16f217322295e64fe3d8
lm9204 <yeondeok@gmail.com>
2018-11-11 20:56:43 +0900

    Edit SpaceSize

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,11 @@ import React from 'react';
 import CommentBox from './CommentBox';
 import CommentList from './CommentList';
 
-
 export default () => {
-    return (
-        <div>
-            <CommentBox />
-            <CommentList />
-        </div>
-    );
+	return (
+		<div>
+			<CommentBox />
+			<CommentList />
+		</div>
+	);
 };
